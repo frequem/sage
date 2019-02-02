e6adbf8cacc43138e7ae407c39bf35dfd20d922a
frequem <frequem@users.noreply.github.com>
2019-02-02 21:21:29 +0100

    added dependencies to sageConfig.cmake

--- sageConfig.cmake
+++ sageConfig.cmake
@@ -1,1 +1,7 @@
 include("${CMAKE_CURRENT_LIST_DIR}/sageTargets.cmake")
+
+find_dependency(SDL2 CONFIG REQUIRED)
+find_dependency(SDL_ttf CONFIG REQUIRED)
+find_dependency(glew CONFIG REQUIRED)
+find_dependency(glm CONFIG REQUIRED)
+find_dependency(soil CONFIG REQUIRED)
