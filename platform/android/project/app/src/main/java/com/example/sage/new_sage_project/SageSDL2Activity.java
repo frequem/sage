d7e67449aedabd7b1c94681930f6dc6765ad08b6
frequem <frequem@users.noreply.github.com>
2019-05-09 22:26:29 +0200

    finalized android debug build

--- platform/android/project/app/src/main/java/com/example/sage/new_sage_project/SageSDL2Activity.java
+++ platform/android/project/app/src/main/java/com/example/sage/new_sage_project/SageSDL2Activity.java
@@ -1,9 +1,9 @@
-package com.example.hellojni;
+package com.example.sage.new_sage_project;
 
 import org.libsdl.app.SDLActivity;
 
 public class SageSDL2Activity extends SDLActivity{
-    private final String LIB_NAME = "sageExample";
+    private final String LIB_NAME = "new_sage_project";
 
 	@Override
 	protected String[] getLibraries(){
